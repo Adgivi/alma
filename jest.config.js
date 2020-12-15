7ed70039be251d9c2eda0fa0a0aa004f500edbe7
adolf.gimeno <adolf.gimeno@mimacom.com>
2020-12-15 20:05:20 +0100

    feat(prettier): install prettier

--- jest.config.js
+++ jest.config.js
@@ -1,20 +1,18 @@
 module.exports = {
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/$1',
-    '^~/(.*)$': '<rootDir>/$1',
-    '^vue$': 'vue/dist/vue.common.js',
-    "assets/(.*)": [
-      "<rootDir>/assets/$1",
-    ]
+    "^@/(.*)$": "<rootDir>/$1",
+    "^~/(.*)$": "<rootDir>/$1",
+    "^vue$": "vue/dist/vue.common.js",
+    "assets/(.*)": ["<rootDir>/assets/$1"]
   },
-  moduleFileExtensions: ['js', 'vue', 'json'],
+  moduleFileExtensions: ["js", "vue", "json"],
   transform: {
-    '^.+\\.js$': 'babel-jest',
-    '.*\\.(vue)$': 'vue-jest',
+    "^.+\\.js$": "babel-jest",
+    ".*\\.(vue)$": "vue-jest"
   },
   collectCoverage: true,
   collectCoverageFrom: [
-    '<rootDir>/components/**/*.vue',
-    '<rootDir>/pages/**/*.vue',
-  ],
-}
+    "<rootDir>/components/**/*.vue",
+    "<rootDir>/pages/**/*.vue"
+  ]
+};
